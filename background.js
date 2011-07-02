c809b9972ab9823659149134ad1e4442f6307b0d
Zhang Chi <clvrobj@gmail.com>
2011-07-02 14:13:25 +0800

    ci

---
+++ background.js
@@ -0,0 +1,264 @@
+// userInfo: name, pwd, isChecked
+var _userInfo = null, keyprefix = 'pbuinfo',
+namekey = keyprefix + 'n', pwdkey = keyprefix + 'p', checkedkey = keyprefix + 'c',
+at = '@', pathBody = 'api.pinboard.in/v1/',
+yesIcon = 'icon_black_19.png', noIcon = 'icon_grey_19.png';
+
+var getMainPath = function () {
+    var userInfo = getUserInfo(),
+    authStr = userInfo.name + ':' + userInfo.pwd;
+    return 'https://' + authStr + at + pathBody;
+};
+
+// {url, title, desc, tag, time, isSaved}
+var pages = {};
+
+var login = function (name, pwd) {
+    // test auth
+    var path = 'https://' + pathBody + 'posts/update';
+    var jqxhr = $.ajax({url: path,
+                        type : 'GET',
+                        dataType: 'json',
+                        crossDomain: true,
+                        contentType:'text/plain'});
+    jqxhr.complete(function (data) {
+                       var res = $(data.responseXML).find('update'),
+                       resTime = res.attr('time');
+                       if (resTime) {
+                           _userInfo.name = name;
+                           _userInfo.pwd = pwd;
+                           _userInfo.isChecked = true;
+                           localStorage[namekey] = name;
+                           localStorage[pwdkey] = pwd;
+                           localStorage[checkedkey] = true;
+                           chrome.tabs.getSelected(
+                               null, function (tab) {
+                                   updatePageInfo(tab.url);
+                               });
+                       } else {
+                           // error
+                           
+                       }
+                   });
+};
+
+var logout = function () {
+    _userInfo.isChecked = false;
+    localStorage.removeItem(checkedkey);
+    localStorage.removeItem(namekey);
+    localStorage.removeItem(pwdkey);
+    var popup = chrome.extension.getViews({type: 'popup'})[0];
+    popup.showLoginWindow();
+};
+
+var getUserInfo = function () {
+    if (!_userInfo) {
+        if (localStorage[checkedkey]) {
+            _userInfo = {isChecked: localStorage[checkedkey],
+                        name: localStorage[namekey],
+                        pwd: localStorage[pwdkey]};
+        } else {
+            _userInfo = {name: '', pwd: '', isChecked: false};
+        }
+    }
+    return _userInfo;
+};
+
+NamedNodeMap.prototype.getAttrVal = function (attrName) {
+    var attr = this.getNamedItem(attrName);
+    if (attr) {
+        return attr.nodeValue;
+    }
+    return null;
+};
+
+// for popup.html to acquire page info
+// if there is no page info at local then get it from server
+var getPageInfo = function (url) {
+    var pageInfo = pages[url];
+    if (pageInfo) {
+        return pageInfo;
+    } else { // download now
+        updatePageInfo(url);
+    }
+    return null;
+};
+
+// refresh page info even page info has fetched from server
+var updatePageInfo = function (url) {
+    var cb = function (pageInfo) {
+        var popup = chrome.extension.getViews({type: 'popup'})[0];
+        popup.renderPageInfo(pageInfo);
+        updateSelectedTabExtIcon();
+    };
+    queryPinState({url: url, ready: cb});
+}
+
+var queryPinState = function (info) {
+    var userInfo = getUserInfo();
+    if (userInfo && userInfo.isChecked && info.url) {
+        var url = info.url;
+        console.log('queryPinState -- ' + url);
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", getMainPath() + 'posts/get?url=' + url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                var post = xhr.responseXML.documentElement.getElementsByTagName("post"), pageInfo;
+                if (post.length > 0) {
+                    
+                    var attrs = post[0].attributes;
+                    pageInfo = {url: attrs.getAttrVal("href"), 
+                                title: attrs.getAttrVal("description"),
+                                desc: attrs.getAttrVal("extended"),
+                                tag: attrs.getAttrVal("tag"),
+                                time: attrs.getAttrVal("time"),
+                                shared: attrs.getAttrVal("shared") == 'no' ? false:true,
+                                toread: attrs.getAttrVal("toread") == 'yes' ? true:false,
+                                isSaved: true};
+                } else {
+                    pageInfo = {isSaved: false};
+                }
+                pages[url] = pageInfo;
+                info.ready && info.ready(pageInfo);
+            }
+        };
+        xhr.send();
+    }
+};
+
+var updateSelectedTabExtIcon = function () {
+    chrome.tabs.getSelected(null, function (tab) {
+                                var pageInfo = pages[tab.url];
+                                console.log('updateSelectedTabExtIcon');
+                                console.log(pageInfo);
+                                var iconPath;
+                                if (pageInfo && pageInfo.isSaved) {
+                                    iconPath = yesIcon;
+                                } else {
+                                    iconPath = noIcon;
+                                }
+                                chrome.browserAction.setIcon(
+                                    {path: iconPath, tabId: tab.id});
+                        });
+};
+
+var addPost = function (info) {
+    var userInfo = getUserInfo();
+    if (userInfo && userInfo.isChecked && info.url && info.title) {
+        console.log('addPost');
+        var path = getMainPath() + 'posts/add?',
+        data = {description: info.title, url: info.url,
+                extended: info.desc, tags: info.tag};
+        info.shared && (data['shared'] = info.shared);
+        info.toread && (data['toread'] = info.toread);
+        var jqxhr = $.ajax({url: path,
+                type : 'GET',
+                dataType: 'json',
+                crossDomain: true,
+                data: data,
+                contentType:'text/plain'});
+        jqxhr.complete(function (data) {
+                           var res = $(data.responseXML).find('result'),
+                           resCode = res.attr('code');
+                           if (resCode == 'done') {
+                               updatePageInfo(info.url);
+                           } else {
+                               // error
+                           }
+                       });
+    }
+};
+
+
+var deletePost = function (url) {
+    var userInfo = getUserInfo();
+    if (userInfo && userInfo.isChecked && url) {
+        console.log('deletePost');
+        var path = getMainPath() + 'posts/delete?';
+        var jqxhr = $.ajax({url: path,
+                type : 'GET',
+                dataType: 'json',
+                crossDomain: true,
+                data: {url: url},
+                contentType:'text/plain'});
+        jqxhr.complete(function (data) {
+                           var res = $(data.responseXML).find('result'),
+                           resCode = res.attr('code');
+                           if (resCode == 'done') {
+                               delete pages[url];
+                               updatePageInfo(url);
+                           } else {
+                               // error
+                           }
+                       });
+    }
+};
+
+var getSuggest = function (url) {
+    var userInfo = getUserInfo();
+    if (userInfo && userInfo.isChecked && url) {
+        console.log('getSuggest');
+        var path = getMainPath() + 'posts/suggest?url=' + url,
+        jqxhr = $.ajax({url: path,
+                type : 'GET',
+                dataType: 'json',
+                crossDomain: true,
+                contentType:'text/plain'});
+        jqxhr.complete(function (data) {
+                           var res = $(data.responseXML).find('popular'),
+                           suggests = [];
+                           for (var i=0, len = res.length; i<len; i++) {
+                               suggests.push($(res[i]).text());
+                           }
+                           var popup = chrome.extension.getViews({type: 'popup'})[0];
+                           popup.renderSuggests(suggests);
+                       });
+    }
+};
+
+chrome.tabs.onUpdated.addListener(
+    function(id, changeInfo, tab) {
+        if (changeInfo.url) {
+            console.log('tabs.onUpdate --' + ' url: ' + changeInfo.url);
+            var url = changeInfo.url;
+            if (!pages[url]) {                
+                chrome.browserAction.setIcon({path: noIcon, tabId: tab.id});
+                queryPinState({url: url,
+                               ready: function (pageInfo) {
+                                   if (pageInfo && pageInfo.isSaved) {
+                                       chrome.browserAction.setIcon(
+                                           {path: yesIcon, tabId: tab.id});
+                                   }
+                               }});
+            }
+        }
+        var url = changeInfo.url || tab.url;
+        if (pages[url] && pages[url].isSaved) {
+            chrome.browserAction.setIcon({path: yesIcon, tabId: tab.id});
+        }
+    });
+
+chrome.tabs.getSelected(null, function (tab) {
+                            queryPinState({url: tab.url,
+                                           ready: function (pageInfo) {
+                                               if (pageInfo && pageInfo.isSaved) {
+                                                   chrome.browserAction.setIcon(
+                                                       {path: yesIcon,
+                                                        tabId: tab.id});
+                                               }
+                                           }});
+                        });
+
+chrome.tabs.onSelectionChanged.addListener(
+    function(tabId, selectInfo) {
+        chrome.tabs.getSelected(null, function (tab) {
+                            queryPinState({url: tab.url,
+                                           ready: function (pageInfo) {
+                                               if (pageInfo && pageInfo.isSaved) {
+                                                   chrome.browserAction.setIcon(
+                                                       {path: yesIcon,
+                                                        tabId: tab.id});
+                                               }
+                                           }});
+                        });
+    });
---
+++ popup.js
@@ -0,0 +1,199 @@
+var bg = chrome.extension.getBackgroundPage(), curTabUrl = '';
+
+var SEC = 1000, MIN = SEC*60, HOUR = MIN*60, DAY = HOUR*24, WEEK = DAY*7;
+Date.prototype.getTimePassed = function () {
+    var ret = {day: 0, hour: 0, min: 0, sec: 0, offset: -1},
+    offset = new Date() - this;
+    if (offset<=0) return ret;
+    ret.offset = offset;
+    ret.week = Math.floor(offset/WEEK); var r = offset%WEEK;
+    ret.day = Math.floor(offset/DAY); var r = offset%DAY;
+    ret.hour = Math.floor(r/HOUR), r = r%HOUR;
+    ret.min = Math.floor(r/MIN), r = r%MIN;
+    ret.sec = Math.floor(r/SEC);
+    return ret;
+};
+
+var renderSuggests = function (suggests) {
+    if (suggests && suggests.length) {
+        var suggestsStr = '';
+        for (var i=0, len = suggests.length; i<len; i++) {
+            suggestsStr = suggestsStr.concat('<a href="#">', suggests[i], '</a>');
+        }
+        $('#suggest').html(suggestsStr);
+        $('#suggest a').click(function (e) {
+                                  var s = $(e.target).text(), t = $('#tag').val();
+                                  if ($.inArray(s, t.split(' ')) == -1) {
+                                      $('#tag').val(t + ' ' + s);
+                                  }
+                              });
+    }
+};
+
+var renderPageInfo = function (pageInfo) {
+    var userInfo = checkLogin();
+    if (userInfo) {
+        hideLoading();
+        if (pageInfo.isSaved) {
+            bg.getSuggest(pageInfo.url);
+            $('#user').text(userInfo.name);
+            pageInfo.url && $('#url').val(pageInfo.url);
+            pageInfo.title && $('#title').val(pageInfo.title);
+            pageInfo.desc && $('#desc').val(pageInfo.desc);
+            pageInfo.tag && $('#tag').val(pageInfo.tag.concat(' '));
+            $('#private').attr('checked', !pageInfo.shared);
+            $('#toread').attr('checked', pageInfo.toread);
+            if (pageInfo.time) {
+                var passed = new Date(pageInfo.time).getTimePassed(),
+                dispStr = 'previously saved ',
+                w = passed.week, d = passed.day, h = passed.hour;
+                if (passed.offset > WEEK) {
+                    dispStr = dispStr.concat(passed.week, ' ', 'days ago');
+                } else if (passed.offset > DAY) {
+                    dispStr = dispStr.concat(passed.day, ' ', 'days ago');
+                } else if (passed.offset > HOUR){
+                    dispStr = dispStr.concat(passed.hour, ' ', 'hours ago');
+                } else {
+                    dispStr = dispStr.concat('just now');
+                }
+                $('.alert').show().html(dispStr);
+                $('#delete').show();
+            }
+        } else {
+            initPopup();
+        }
+        $('#tag').focus();
+    }
+};
+
+var getPageInfo = function (url) {
+    var cb = function (pageInfo) {
+        renderPageInfo(pageInfo);
+    };
+    var pageInfo = bg.getPageInfo(url);
+    if (pageInfo) {
+        renderPageInfo(pageInfo);
+    } else {
+        showLoading();
+    }
+};
+
+var checkLogin = function () {
+    var userInfo = bg.getUserInfo();
+    if (!userInfo || !userInfo.isChecked) {
+        showLoginWindow();        
+        return false;
+    } else {
+        hideLoginWindow();
+        return userInfo;
+    }
+};
+
+var initPopup = function () {
+    var userInfo = checkLogin();
+    if (userInfo) {
+        $('#user').text(userInfo.name);
+        $('#user').unbind('click').click(
+            function () {
+                chrome.tabs.create(
+                    {url: 'https://pinboard.in/u:'+userInfo.name});
+                var popup = chrome.extension.getViews(
+                    {type: 'popup'})[0];
+                popup && popup.close();
+            });
+        chrome.tabs.getSelected(
+            null, function (tab) {
+                $('#url').val(tab.url);
+                $('#title').val(tab.title);
+                $('#desc').val('');
+                $('#tags').val('');
+                $('#private').attr('checked', false);
+                $('#toread').attr('checked', false);
+                $('.alert').hide();
+                $('#delete').hide();
+                $('.confirm').hide();
+                $('logo-unlogin').hide();
+                bg.getSuggest(tab.url);
+            });
+    }
+};
+
+var showLoading = function () {
+    $('#state-mask').show();
+},
+hideLoading = function () {
+    $('#state-mask').hide();
+};
+
+var showLoginWindow = function () {
+    $('.logo-unlogin').show();
+    $('#login-window').show();
+    var submit = function () {
+        showLoading();
+        var name = $('#login-window #username').val(),
+        pwd = $('#login-window #password').val();
+        bg.login(name, pwd);
+    };
+    $('#login-window #username').unbind('keypress').keypress(
+        function (e) {
+            var code = e.keyCode || e.which;
+            if(code == 13) {
+                submit();
+            }
+        });
+    $('#login-window #password').unbind('keypress').keypress(
+        function (e) {
+            var code = e.keyCode || e.which;
+            if(code == 13) {
+                submit();
+            }
+        });
+    $('#login-window #login').unbind('click').click(submit);
+},
+hideLoginWindow = function () {
+    $('.logo-unlogin').hide();
+    $('#login-window').hide();
+};
+
+$(function () {
+      initPopup();
+      if (checkLogin()) {
+          chrome.tabs.getSelected(
+              null, function (tab) {
+                  getPageInfo(tab.url);
+              });
+      }
+      $('#submit').click(
+          function () {
+              showLoading();
+              var info = {},
+              url = $('#url').val(), title = $('#title').val(),
+              desc = $('#desc').val(), tag = $('#tag').val();
+              url && (info.url = url);
+              title && (info.title = title);
+              desc && (info.desc = desc);
+              tag && (info.tag = tag);
+              $('#private').attr('checked') && (info.shared = 'no');
+              $('#toread').attr('checked') && (info.toread = 'yes');
+              console.log(bg.addPost);
+              bg.addPost(info);
+          });
+      $('#delete').click(function () {
+                             $(this).hide();
+                             $('.confirm').show();
+                         });
+      $('#cancel-delete').click(function () {
+                                    $('.confirm').hide();
+                                    $('#delete').show();
+                                });
+      $('#destroy').click(function () {
+                             showLoading();
+                             chrome.tabs.getSelected(
+                                 null, function (tab) {
+                                     bg.deletePost(tab.url);
+                                 });
+                         });
+      $('.logout').click(function () {
+                             bg.logout();
+                         });
+  });
