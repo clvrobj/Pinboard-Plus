9339bf10387beaa77a775121ccac4caaea52cca3
Chi <clvrobj@gmail.com>
2013-07-27 23:08:00 +0800

    mv img to img folder

--- common.js
+++ common.js
@@ -3,5 +3,5 @@ var _userInfo = null, _tags = [], keyprefix = 'pbuinfo',
 namekey = keyprefix + 'n', pwdkey = keyprefix + 'p', checkedkey = keyprefix + 'c',
 nopingkey = keyprefix + 'np', // config in the settings for not checking page pin state
 mainPath = 'https://api.pinboard.in/v1/',
-yesIcon = 'icon_colored_19.png', noIcon = 'icon_grey_19.png', savingIcon = 'icon_grey_saving_19.png';
+yesIcon = '/img/icon_colored_19.png', noIcon = '/img/icon_grey_19.png', savingIcon = '/img/icon_grey_saving_19.png';
 var REQ_TIME_OUT = 125 * 1000;
