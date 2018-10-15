b91ea0a7659006c33d0e4a2444b80a06c38a1c6e
Ventz Petkov <ventz@vpetkov.net>
2018-10-15 05:00:47 -0400

    Feature Add: utilizing local meta keywords tags to suggest better and more relevant tags. Examples include: nytimes.com and washingtonpost.com

---
+++ app/scripts/keywords_suggestions.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse) {
+    if (message.method == "getKeywordsSuggestionTags") {
+	  var metas = document.getElementsByTagName('meta');
+	    for (i=0; i < metas.length; i++) {  
+		  if (metas[i].getAttribute("name") === "keywords") {
+			var tags = metas[i].getAttribute("content").toLowerCase().replace(/,/g, ' ').replace(/;/, '').split(/(\s+)/).filter(function(e) { return e.trim().length > 0; }).sort().filter(function(item, pos, ary) {return !pos || item != ary[pos - 1];});
+			break
+            console.log("Tags: " + tags);
+		  }
+		}
+        sendResponse({data: tags});
+    }
+});
